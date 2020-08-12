0fd9f264286735e09c1fd45f97c682a6988910ab
Baumel Márton <marcibaumel@gmail.com>
2020-08-12 16:08:59 +0200

    f

---
+++ EvoRPGDemo/Models/FightSystem/FightSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoRPGDemo.Models.FightSystem
+{
+    class FightSystem: IFightSystem
+    {
+      
+
+        void IFightSystem.Attack1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EvoRPGDemo/Models/FightSystem/IFightSystem.cs
+++ EvoRPGDemo/Models/FightSystem/IFightSystem.cs
@@ -5,16 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EvoRPGDemo.Models.IFightSystem
+namespace EvoRPGDemo.Models.FightSystem
 {
-    interface IFightSystem<Player>
+    interface IFightSystem
     {
         /*Azt akarom itt megoldani, hogy
          * mikor a használó osztály valamilyen függvényt hivan meg
          * akkor az innen történjen, hogy miket is kell haználnia.
          */
 
-        void Attack1(Player player, Enemy enemy1, Enemy enemy2);
+        void Attack1();
+
+
 
     }
 }
