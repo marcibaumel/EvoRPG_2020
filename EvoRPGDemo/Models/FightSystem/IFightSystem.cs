07527923e14a51d6f2f902fc41823700d69c0d4c
Baumel Márton <marcibaumel@gmail.com>
2020-08-12 13:46:21 +0200

    Interface concept kialakítás

---
+++ EvoRPGDemo/Models/FightSystem/IFightSystem.cs
@@ -0,0 +1,20 @@
+﻿using EvoRPGDemo.Classes.Enemys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoRPGDemo.Models.IFightSystem
+{
+    interface IFightSystem<Player>
+    {
+        /*Azt akarom itt megoldani, hogy
+         * mikor a használó osztály valamilyen függvényt hivan meg
+         * akkor az innen történjen, hogy miket is kell haználnia.
+         */
+
+        void Attack1(Player player, Enemy enemy1, Enemy enemy2);
+
+    }
+}
